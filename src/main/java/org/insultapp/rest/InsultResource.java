b9d21392930644b27961e1bc04ebea206264f34d
Roger P. Nelson <roger.nelson@rbc.com>
2018-02-27 09:56:18 -0600

    maven resource filtering

--- src/main/java/org/insultapp/rest/InsultResource.java
+++ src/main/java/org/insultapp/rest/InsultResource.java
@@ -1,7 +1,10 @@
 package org.insultapp.rest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Properties;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -33,12 +36,15 @@ public class InsultResource {
 	@GET
 	@Produces(value = MediaType.TEXT_HTML)
 	public String insultHtml() {
+		String buildTime= getBuildTime();
 		return "<html><body><h1>Insult " + ++_visits + "!</h1>"
 				+ "You requested an insult @"
 				+ new SimpleDateFormat("yyyy.MM.dd HH:mm:ss").format(new Date())
 				+"<hr/><b>"
 				+ insultGenerator.insult()
-				+ "</b></body>";
+				+ "</b><br/>"
+				+ "<i>" + buildTime + "</i>"
+				+ "</body>";
 	}
 
 	@Path("/{name}")
@@ -47,4 +53,20 @@ public class InsultResource {
 	public String namedInsult(@PathParam("name") String name) {
 		return insultGenerator.namedInsult(name);
 	}
-}
+	
+	private String getBuildTime() {
+		String buildTime= "n/a";
+	ClassLoader classLoader = this.getClass().getClassLoader();
+	Properties prop = new Properties();
+	try {
+		InputStream stream= classLoader.getResourceAsStream("build.properties"); 
+	    prop.load(stream);
+	    buildTime = prop.getProperty("build.date");
+	    String pomVersion = prop.getProperty("version");
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    buildTime= e.getMessage();
+	}
+	return buildTime;
+	}
+	}
